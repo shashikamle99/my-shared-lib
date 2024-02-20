cc159b882ae262a778d6f5a06c6b867acdd1d051
shashikamle99 <154310776+shashikamle99@users.noreply.github.com>
2024-02-20 15:46:29 +0530

    Update checkoutCode.groovy

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -2,5 +2,5 @@ def call(String repoUrl, String branch){
    def workingDir = "${env.WORKSPACE}"
    sh "git clone ${repoUrl} ${workingDir}"
    sh "git checkout ${branch}"
-   return workingDir
+   // return workingDir
 }
